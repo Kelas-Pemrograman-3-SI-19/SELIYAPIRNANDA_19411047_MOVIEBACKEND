b6e46f782cc8b7886714a68ebdc882d2e0c6db85
seliyapirnanda <seliyapirnanda92@gmail.com>
2021-07-17 14:59:37 +0700

    movie

---
+++ model/Order.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const objectId = mongoose.Schema.ObjectId
+
+const OrderSchema = new Schema({
+    idUser: {
+      type: objectId
+    },
+    idFilm: {
+      type: objectId
+    },
+    judulFilm: {
+      type: String
+    },
+    harga: {
+      type: Number
+    },
+    jumlah: {
+      type: Number
+    },
+    total: {
+      type: Number
+    },
+    image: {
+        type: String
+    },
+    // 1 = belum verifikasi, 2 = sedang dalam pengiriman, 3 = sudah di terima
+    status: {
+        type: Number,
+        default: 1
+    }
+})
+
+module.exports = mongoose.model('order', OrderSchema)
